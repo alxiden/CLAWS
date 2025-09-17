66f91876a151db106e5773764d78d0b7a2b25f0b
Daniel <github.com.rectify325@passmail.net>
2025-09-17 17:06:54 +0100

    Update inputs.rs

--- src/functions/inputs.rs
+++ src/functions/inputs.rs
@@ -7,7 +7,7 @@ pub fn get_user_input(prompt: &str) -> String {
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
-    input.trim().to_string();
+    input = input.trim().to_string();
 
-    return input;
+    input
 }
